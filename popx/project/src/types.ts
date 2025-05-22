1b89c0701b79262e7c356234752b787049385ab0
surya0556 <160869664+surya0556@users.noreply.github.com>
2025-05-22 18:11:29 +0530

    Delete popx/project directory

--- popx/project/src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  fullName: string;
-  phoneNumber: string;
-  email: string;
-  password: string;
-  companyName: string;
-  isAgency: boolean;
-  profilePicture?: string;
-}
